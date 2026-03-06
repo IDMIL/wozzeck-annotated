7ec426e69804e9e8ea5c2ff9357b31253d422984
Arden Butterfield <76457489+ArdenButterfield@users.noreply.github.com>
2026-03-06 14:25:47 -0500

    build all pages

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from 'node:path';
 
 export default defineConfig({
   root: ".",
   build: {
+      rollupOptions: {
+          input: {
+              main: resolve(__dirname, 'index.html'),
+              // Add other HTML files here
+              french: resolve(__dirname, 'fr.html'),
+              english: resolve(__dirname, 'en.html'),
+          },
+      },
     outDir: "dist",
   },
 });
