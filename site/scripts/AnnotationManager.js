89337f63ee943fcfb1ec3740acd13a320ccc1b8b
Arden Butterfield <76457489+ArdenButterfield@users.noreply.github.com>
2026-03-05 20:40:04 -0500

    select annotation types

--- site/scripts/AnnotationManager.js
+++ site/scripts/AnnotationManager.js
@@ -1,4 +1,15 @@
 class AnnotationManager extends TimeManagerListener {
+  annotationCodes = {
+    'dy' : 'Dynamiques',
+    'du': 'Durée',
+    'for' : 'Formes',
+    'int' : 'Intonation',
+    'mo' : 'Motifs',
+    'tim' : 'Timbre'
+  }
+
+  soloedAnnotationCategories = [];
+
   constructor(timeManager) {
     super();
 
@@ -6,13 +17,38 @@ class AnnotationManager extends TimeManagerListener {
 
     let annotationsSection = document.getElementById('annotations-section');
 
+    let annotationTypeSelectorDiv = document.createElement('div');
+    annotationTypeSelectorDiv.id = 'annotation-type-selectors';
+
+    for (const code in this.annotationCodes) {
+      let codeButton = document.createElement('button');
+      codeButton.innerText = this.annotationCodes[code];
+      codeButton.classList.add('annotation-type');
+      codeButton.classList.add(code + '-annotation-type');
+      codeButton.onclick = (event) => {
+        console.log(event.shiftKey);
+        if (event.shiftKey && !this.soloedAnnotationCategories.includes(code)) {
+          this.soloedAnnotationCategories.push(code);
+        } else if (event.shiftKey) {
+          return;
+        } else if (this.soloedAnnotationCategories.includes(code)) {
+          this.soloedAnnotationCategories = this.soloedAnnotationCategories.filter(item => item !== code);
+        } else {
+          this.soloedAnnotationCategories = [code];
+        }
+        this.setAnnotationVisibilityFromState();
+      }
+      annotationTypeSelectorDiv.appendChild(codeButton);
+    }
+
+    annotationsSection.appendChild(annotationTypeSelectorDiv);
+
     let scrollerDiv = document.createElement('div');
     scrollerDiv.id = 'annotations-scroller';
     scrollerDiv.classList.add('scroller-area');
     annotationsSection.appendChild(scrollerDiv);
 
     for (const annotation of annotations) {
-      console.log(annotation);
       let annotationDiv = document.createElement("div");
       annotationDiv.classList.add("annotation");
       annotationDiv.classList.add(annotation.code + "annotation");
@@ -27,7 +63,7 @@ class AnnotationManager extends TimeManagerListener {
       annotationDiv.appendChild(annotationTextDiv);
 
       annotationDiv.onclick = () => {
-        timeManager.goToTime(annotation.act, annotation.measure_range[0], 1);
+        this.timeManager.goToTime(annotation.act, annotation.measure_range[0], 1);
       }
 
       scrollerDiv.appendChild(annotationDiv);
@@ -38,6 +74,41 @@ class AnnotationManager extends TimeManagerListener {
     return 'Act ' + annotation.act + ', scene ' +
       this.timeManager.getScene(annotation.act, annotation.measure_range[0]) +
       ', measures ' + annotation.measure_range[0] + '–' + annotation.measure_range[1];
+  }
+
+  setAnnotationVisibilityFromState() {
+    if (this.soloedAnnotationCategories.length === 0) {
+      for (const elem of document.getElementsByClassName('annotation-type')) {
+        elem.classList.remove('annotation-type-hidden');
+      }
+
+      for (const elem of document.getElementsByClassName('annotation')) {
+        elem.classList.remove('annotation-hidden');
+      }
+      return;
+    }
+
+    for (const c in this.annotationCodes) {
+      for (const elem of document.getElementsByClassName(c + '-annotation-type')) {
+        if (this.soloedAnnotationCategories.includes(c)) {
+          elem.classList.remove('annotation-type-hidden');
+        } else {
+          elem.classList.add('annotation-type-hidden');
+        }
+      }
+    }
+
+    for (const c in this.annotationCodes) {
+      if (this.soloedAnnotationCategories.includes(c)) {
+        for (const elem of document.getElementsByClassName(c + '-annotation')) {
+          elem.classList.remove('annotation-hidden');
+        }
+      } else {
+        for (const elem of document.getElementsByClassName(c + '-annotation')) {
+          elem.classList.add('annotation-hidden');
+        }
+      }
+    }
   }
 
   timeUpdated(scoreTime) {
